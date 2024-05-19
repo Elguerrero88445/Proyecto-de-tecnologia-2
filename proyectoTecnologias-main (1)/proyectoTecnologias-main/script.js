7a9fa9037f9d1fae1f89b3183041f358273e089c
Elguerrero88445 <145805894+Elguerrero88445@users.noreply.github.com>
2024-05-19 00:13:12 -0600

    Add files via upload

---
+++ proyectoTecnologias-main (1)/proyectoTecnologias-main/script.js
@@ -0,0 +1,40 @@
+///funciones para referir a links
+function iniciar(){
+  location.href="preguntas1.html";
+}
+function home(){
+  location.href="index.html";
+}
+function cuceiF(){
+  location.href="https://www.facebook.com/udegcucei/?locale=es_LA";
+}
+function cuceiW(){
+  location.href="http://www.cucei.udg.mx/";
+}
+function cuceaF(){
+  location.href="https://www.facebook.com/cucea.oficial/?locale=es_LA";
+}
+function cuceaW(){
+  location.href="https://www.cucea.udg.mx/";
+}
+function cucadF(){
+  location.href="https://es-la.facebook.com/CUAAD/";
+}
+function cucadW(){
+  location.href="https://cuaad.udg.mx/";
+}
+function cucsF(){
+  location.href="https://www.facebook.com/CUCSUdeG/?locale=es_LA";
+}
+function cucsW(){
+  location.href="https://www.cucs.udg.mx/";
+}
+function cucshW(){
+  location.href="http://www.cucsh.udg.mx/";
+}
+function udgF(){
+  location.href="https://www.facebook.com/udg.mx/?locale=es_LA";
+}
+function udgW(){
+  location.href="https://www.udg.mx/es";
+}
